4131068fdba0a6b67347876726850a7b127dc578
BoilingSoup <boilingsoup@protonmail.com>
2022-11-05 17:22:02 -0700

    silent keymap NvimTreeToggle

--- lua/bsoup/core/keymaps.lua
+++ lua/bsoup/core/keymaps.lua
@@ -21,7 +21,7 @@ keymap.set("n", "<C-l>", "<C-w>l")
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
 -- nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
